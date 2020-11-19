786f25a0182cf2e8e9505e2612c38e0ddbd455da
Quinn O'Neill <quinnoneill@Quinns-MacBook-Pro-2.local>
2020-11-18 17:37:20 -0700

    first commit

---
+++ wp-content/themes/dash/homepage-template.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ *  Template Name: Homepage
+ *
+ *
+ * @package Dash
+ */
+get_header('custom');
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<div class="hero">
+				<div class="hero-bg">
+					<div class="left-img">
+						<img src="/wp-content/uploads/2020/11/rock-staar-ccHVqD5P-8g-unsplash-scaled.jpg">
+					</div>
+					<div class="right-img">
+						<img src="/wp-content/uploads/2020/11/nathan-dumlao-qUW3H5S4Qx8-unsplash-scaled.jpg">
+					</div>
+					<h1 class="top">DIGITAL</h1>
+					<h1 class="bottom">DASH</h1>
+					<hr class="gold">
+					<h2 class="hero-h2">Digital Marketing</h2>
+					<hr class="gold-top">
+					<p class="hunnit">100</p>
+					<img class="top-left" src="/wp-content/uploads/2020/11/dash-left-corner-8.png">
+					<div class="navvy">
+						<div class="menu01">
+							<ul>
+								<li>ABOUT</li>
+								<li>SERVICES</li>
+								<li>CLIENTS</li>
+								<li>CONTACT</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+</body>
